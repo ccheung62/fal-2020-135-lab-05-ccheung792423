0e494c0bc7a9310f1d107f48b4424518dd852586
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-30 21:48:47 -0400

    task a

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include "funcs.h"
 
-// add functions here
+//Task A
+bool isDivisibleBy(int n, int d) {
+  if (d==0){
+    return false; //can't divide by zero 
+  }
+  if (n%d==0){
+    return true; //no remainder so it is divisible
+  }
+  else {
+    return false;
+  }
+}
+
+//Task B
+
 
--- tests.cpp
+++ tests.cpp
@@ -2,4 +2,21 @@
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+//Task A
+TEST_CASE ("Regular cases"){
+  CHECK(isDivisibleBy(4,5)==false);
+  CHECK(isDivisibleBy(125,25)==true);
+  CHECK(isDivisibleBy(8,3)==false);
+}
+TEST_CASE ("negative numbers"){
+  CHECK(isDivisibleBy(-100,-25)==true);
+  CHECK(isDivisibleBy(-178,-3)==false);
+  CHECK(isDivisibleBy(-2,-3)==false);
+}
+TEST_CASE("zero"){
+  CHECK(isDivisibleBy(0,100)==true);
+  CHECK(isDivisibleBy(100,0)==false);
+}
+
+//Task B
+
